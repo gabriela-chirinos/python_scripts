4357d5dd19b37619346eae5d1d92e9bb709dc45d
Gabriela-Chirinos <chirinos@outlook.com>
2026-05-27 12:33:54 -0700

    made a commit

---
+++ bananas.py
@@ -0,0 +1,11 @@
+bananas = 2
+
+if bananas >= 5:
+    print("I have a large bunch of bananas")
+elif bananas >= 1:
+    print("I have a small bunch of bananas")
+else:
+    print("I don't have any bananas")
+
+
+
