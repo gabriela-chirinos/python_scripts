b514a2bd2b6893fac2638a22452cf93236829bdf
Gabriela-Chirinos <chirinos@outlook.com>
2026-05-29 10:16:59 -0700

    practiced math.floor

---
+++ mathfloor.py
@@ -0,0 +1,12 @@
+import math
+
+
+decimalOne = float(input("Enter a decimal for this to work: "))
+
+decimalTwo = float(input("Enter another decimal to finish: "))
+
+
+difference = math.floor(decimalOne + decimalTwo) 
+
+
+print(f"The answer is {difference}")
